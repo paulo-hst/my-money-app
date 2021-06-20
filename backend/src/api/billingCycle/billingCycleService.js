502cbf7b4aff850a02243fbd4d30238f0b0a236f
hst.paulo <hst.paulo@gmail.com>
2021-06-20 14:53:02 -0300

    obtendo quantidade de registros

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -2,7 +2,34 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-// BillingCycle.updateOptions({ new: true, runValidators: true }) // retorna sempre o objeto novo
+BillingCycle.updateOptions({ new: true, runValidators: true }) // retorna sempre o objeto novo
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        // parâmetro value: valor inteiro
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
+// BillingCycle.route('summary', (req, res, next) => {
+//     BillingCycle.aggregate([{ 
+//         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+//     }, { 
+//         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+//     }, { 
+//         $project: {_id: 0, credit: 1, debt: 1}
+//     }], (error, result) => {
+//         if(error) {
+//             res.status(500).json({errors: [error]})
+//         } else {
+//             res.json(result[0] || {credit: 0, debt: 0})
+//         }
+//     })
+// })
 
 // BillingCycle.route('get', (req, res, next) => {
 //     BillingCycle.find({}, (err,docs) {
