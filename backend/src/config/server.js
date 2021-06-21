10e0ae5131a38e303c667fbb4fc90e00319e1507
hst.paulo <hst.paulo@gmail.com>
2021-06-21 20:15:38 -0300

    habilitando o middleware query parser para paginacao

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,12 +2,14 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
-const allowCors = require('./cors')
 const server = express() // instancia do express
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true })) // body parser interpreta caso seja url encoded
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser()) // permite paginação
 
 server.listen(port, function(){
     console.log(`Servidor rodando na porta ${port}`)
