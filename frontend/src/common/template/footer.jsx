742c09d43e83999a03e702624fd0f41ca3cb976c
hst.paulo <hst.paulo@gmail.com>
2021-06-23 19:58:29 -0300

    navegacao com o react router

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'>
         <strong>
-            <p>Paulo Teixeira | 2021    
+            <p>Paulo Teixeira | 2021
                 <a href='http://github.com/paulo-hst/' target='_blank'>
                     GitHub
                 </a>
