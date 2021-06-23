61e053a9173f509ac51ae7ada9d555516a25f528
hst.paulo <hst.paulo@gmail.com>
2021-06-23 19:33:35 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            <p>Paulo Teixeira | 2021    
+                <a href='http://github.com/paulo-hst/' target='_blank'>
+                    GitHub
+                </a>
+            </p>
+            
+        </strong>
+    </footer>
+)
