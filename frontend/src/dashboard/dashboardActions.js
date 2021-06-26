37f21eca4468a51b9b0a3990a53d3e578e16aa9e
hst.paulo <hst.paulo@gmail.com>
2021-06-25 21:23:34 -0300

    criando arquivo api para requisicoes com axios

--- frontend/src/dashboard/dashboardActions.js
+++ frontend/src/dashboard/dashboardActions.js
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import api from '../services/api'
 
 const BASE_URL = 'http://localhost:3003/api'
 
 // action creator
 export function getSummary(){
-    const request = axios.get(`${BASE_URL}/billingCycles/summary`)
+    const request = api.get('/billingCycles/summary')
     
     return {
         type: 'BILLING_SUMMARY_FETCHED',
