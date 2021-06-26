538ea6118c0c46f233d64378653bac0603715b11
hst.paulo <hst.paulo@gmail.com>
2021-06-26 20:44:16 -0300

    fix

--- frontend/src/dashboard/dashboardActions.js
+++ frontend/src/dashboard/dashboardActions.js
@@ -1,7 +1,5 @@
 import api from '../services/api'
 
-const BASE_URL = 'http://localhost:3003/api'
-
 // action creator
 export function getSummary(){
     const request = api.get('/billingCycles/summary')
