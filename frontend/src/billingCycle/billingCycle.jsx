e728d43377a6aa9b5c149c91fe2bd20388e7d348
hst.paulo <hst.paulo@gmail.com>
2021-06-29 20:04:15 -0300

    componente billing cycle list

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -12,6 +12,8 @@ import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 
+import List from './billingCycleList'
+
 class BillingCycle extends Component{
     
     componentDidMount(){
@@ -34,7 +36,9 @@ class BillingCycle extends Component{
                         </TabsHeader>
                         
                         <TabsContent>
-                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
                             <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
---
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class BillingCycleList extends Component{
+    render(){
+        return(
+            <div className='table'>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Mês</th>
+                        <th>Ano</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </div>
+        )
+    }
+}
+
+export default BillingCycleList
