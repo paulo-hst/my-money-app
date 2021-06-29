9dd7fea240dc813ea29c6213d576482e33efe93f
hst.paulo <hst.paulo@gmail.com>
2021-06-29 19:31:31 -0300

    visibilidade das abas

--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -3,17 +3,22 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import If from '../operator/if'
+
 class TabContent extends Component{
     render(){
 
         // tab selecionada
         const selected = this.props.tab.selected === this.props.id
+        const visible = this.props.tab.visible[this.props.id]
 
         return(
-            // compara se a tab estiver ativa ou não
-            <div id={this.props.id} className={`tab-pane ${selected ? 'active': ''}`}>
-                {this.props.children}
-            </div>
+            <If test={visible}>
+                {/* compara se a tab estiver ativa ou não */}
+                <div id={this.props.id} className={`tab-pane ${selected ? 'active': ''}`}>
+                    {this.props.children}
+                </div>
+            </If>
         )
     }
 }
