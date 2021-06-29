f189c0ea6cb292922ef24189e50e4df44ef0b579
hst.paulo <hst.paulo@gmail.com>
2021-06-29 19:57:16 -0300

    criando action getList e reducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,11 +1,13 @@
-import { combineReducers } from "redux"
+import { combineReducers } from 'redux'
 
-import DashboardReducer from "../dashboard/dashboardReducer"
-import TabReducer from "../common/tab/tabReducer"
+import DashboardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
