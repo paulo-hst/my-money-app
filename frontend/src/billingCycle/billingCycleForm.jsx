9b8df08b84b58f143211ecd4effccb42c59de311
hst.paulo <hst.paulo@gmail.com>
2021-07-04 14:13:54 -0300

    adicionando o campo status no item list

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,7 +10,7 @@ import ItemList from './itemList'
 class BillingCycleForm extends Component{
     render(){
 
-        const { handleSubmit, readOnly, credits } = this.props
+        const { handleSubmit, readOnly, credits, debts } = this.props
 
         return(
             <form role='form' onSubmit={handleSubmit}>
@@ -48,6 +48,14 @@ class BillingCycleForm extends Component{
                         field='credits'
                         legend='Créditos'
                     />
+                    <ItemList 
+                        cols='12 6' 
+                        list={debts} 
+                        readOnly={readOnly}
+                        field='debts'
+                        legend='Débitos'
+                        showStatus={true}
+                    />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
@@ -65,6 +73,9 @@ class BillingCycleForm extends Component{
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
 const selector = formValueSelector('billingCycleForm')
 
-const mapStateToProps = state => ({credits: selector(state, 'credits')})
+const mapStateToProps = state => ({
+    credits: selector(state, 'credits'),
+    debts: selector(state, 'debts')
+})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
 
 class ItemList extends Component{
 
@@ -40,6 +41,15 @@ class ItemList extends Component{
                         placeholder='Informe o valor' 
                         readOnly={this.props.readOnly}/>
                 </td>
+                
+                <If test={this.props.showStatus}>
+                    <Field 
+                        name={`${this.props.field}[${index}].status`}  
+                        component={Input} 
+                        placeholder='Informe o status' 
+                        readOnly={this.props.readOnly}/>
+                </If>
+
                 <td>
                     <button
                         type='button' 
@@ -78,6 +88,9 @@ class ItemList extends Component{
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className='table-actions'>Ações</th>
                             </tr>
                         </thead>
