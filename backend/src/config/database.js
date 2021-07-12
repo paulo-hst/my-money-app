bd92bc757e26a360ea2da93ff69aedb33b088470
Paulo Teixeira <hst.paulo@gmail.com>
2021-07-11 21:19:37 -0300

    deploy heroku

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb+srv://paulo:<password>@cluster0.ygfoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
 
 // mensagem de erro ao retornar um post
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
